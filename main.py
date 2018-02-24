84c4e25659df36ba82586b2c07d40f387f19f933
Pavel Durmanov <slava3221852@gmail.com>
2018-02-24 14:54:16 +0300

    fixed delete function

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ def accept_order(message):
     if message.reply_to_message.forward_from and message.from_user.id in moderators:
         bot.forward_message(chat_id='@tgram_jobs', from_chat_id=message.chat.id,
                             message_id=message.reply_to_message.message_id)
-        bot.send_message(
-            chat_id=message.reply_to_message.forward_from.id, text='Ваша заявка опубликована.')
+        bot.send_message(chat_id=message.reply_to_message.forward_from.id, text='Ваша заявка опубликована.')
 
 
 @bot.message_handler(commands=['delete'], func=lambda m: m.reply_to_message)
 def delete_order(message):
     reason = message.text.split(maxsplit=1)
-    if len(reason) > 1 and message.reply_to_message.forward_from and message.from_user.id in moderators:
+    info_message = reason[1] if len(reason) > 1 else 'Без указания причины.'
+    if message.reply_to_message.forward_from and message.from_user.id in moderators:
         bot.delete_message(
             message.chat.id, message.reply_to_message.message_id)
         bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(
-            chat_id=message.reply_to_message.forward_from.id, text=f'Ваша заявка удалена.\n{reason[1]}')
+        bot.send_message(chat_id=message.reply_to_message.forward_from.id,
+                         text=f'Ваша заявка удалена.\n{info_message}')
 
 
 @bot.message_handler(content_types=['text', 'photo', 'video', 'document'], func=lambda m: m.chat.type == 'private')
